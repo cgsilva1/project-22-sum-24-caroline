e11f79dc0b13d2773b389be1c65610d7bc3c73b7
Caroline Silva <cgsilva@usc.edu>
2022-06-03 13:02:15 -0400

    Need to fix redirection

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/user1')
+def user1():
+    return render_template('userPage1.html', title="Carolines Info", url=os.getenv("URL"))
+
+@app.route('/<user2>')
+def user2(user2):
+    return render_template('userPage2.html', title="Sree Info", url=os.getenv("URL"))
